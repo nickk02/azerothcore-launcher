4e45e895fe9442188c7072a710fdfa003eb67537
Nicolas Sanchez <98576999+nickk02@users.noreply.github.com>
2026-07-30 18:30:01 -0500

    Add RealmConfig settings persistence

---
+++ Core/RealmConfig.cpp
@@ -0,0 +1,52 @@
+#include "RealmConfig.h"
+#include <fstream>
+#include <sstream>
+
+namespace Core
+{
+    std::filesystem::path RealmConfig::ConfigDir()
+    {
+        wchar_t* appdata = nullptr;
+        size_t len = 0;
+        _wdupenv_s(&appdata, &len, L"APPDATA");
+        std::filesystem::path dir = std::filesystem::path(appdata ? appdata : L".") / L"AzerothCore";
+        free(appdata);
+        std::filesystem::create_directories(dir);
+        return dir;
+    }
+
+    RealmConfig RealmConfig::Load()
+    {
+        RealmConfig cfg;
+        std::filesystem::path file = ConfigDir() / L"settings.ini";
+        std::wifstream in(file);
+        if (!in.is_open())
+            return cfg;
+
+        std::wstring line;
+        while (std::getline(in, line))
+        {
+            auto eq = line.find(L'=');
+            if (eq == std::wstring::npos)
+                continue;
+            std::wstring key = line.substr(0, eq);
+            std::wstring value = line.substr(eq + 1);
+            if (key == L"WowPath")
+                cfg.WowPath = value;
+            else if (key == L"RealmAddress")
+                cfg.RealmAddress = value;
+            else if (key == L"CredentialVaultEnabled")
+                cfg.CredentialVaultEnabled = (value == L"1");
+        }
+        return cfg;
+    }
+
+    void RealmConfig::Save() const
+    {
+        std::filesystem::path file = ConfigDir() / L"settings.ini";
+        std::wofstream out(file, std::ios::trunc);
+        out << L"WowPath=" << WowPath << L"\n";
+        out << L"RealmAddress=" << RealmAddress << L"\n";
+        out << L"CredentialVaultEnabled=" << (CredentialVaultEnabled ? L"1" : L"0") << L"\n";
+    }
+}
