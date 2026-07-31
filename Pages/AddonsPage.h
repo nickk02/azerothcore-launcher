27191bd6a37439b4eb9076fedb2b48c4bfd69058
Nick <98576999+nickk02@users.noreply.github.com>
2026-07-31 05:59:09 -0500

    Build out AddonsPage (#16)
    * Build out AddonsPage: search, results, open-folder shortcut
    
    * Remove em dash from addon result display string

--- Pages/AddonsPage.h
+++ Pages/AddonsPage.h
@@ -1,11 +1,20 @@
 #pragma once
 #include "Pages/AddonsPage.g.h"
+#include "../Core/AddonCatalog.h"
 
 namespace winrt::AzerothCore::Pages::implementation
 {
     struct AddonsPage : AddonsPageT<AddonsPage>
     {
-        AddonsPage() { InitializeComponent(); }
+        AddonsPage();
+
+        void SearchButton_Click(winrt::Windows::Foundation::IInspectable const&, winrt::Microsoft::UI::Xaml::RoutedEventArgs const&);
+        void SearchBox_KeyDown(winrt::Windows::Foundation::IInspectable const&, winrt::Microsoft::UI::Xaml::Input::KeyRoutedEventArgs const&);
+        void OpenFolderLink_Click(winrt::Windows::Foundation::IInspectable const&, winrt::Microsoft::UI::Xaml::RoutedEventArgs const&);
+        winrt::fire_and_forget RunSearchAsync(std::wstring query);
+
+    private:
+        Core::AddonCatalog m_catalog;
     };
 }
 
